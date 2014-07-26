109262c9277dd2f40d01617ac7fa02e93a16b477
ranga <ranga@ranga-home-ubuntu>
2014-07-26 17:56:45 +0530

    Changes to display Organization name

--- swing-sample-treetable-withroot/src/main/java/com/blogspot/javanbswing/treetable/withroot/MyTreeTableModel.java
+++ swing-sample-treetable-withroot/src/main/java/com/blogspot/javanbswing/treetable/withroot/MyTreeTableModel.java
@@ -22,7 +22,7 @@ public class MyTreeTableModel extends AbstractTreeTableModel {
     @Override
     public Object getValueAt(Object node, int column) {
         if (node instanceof Organization) {
-            if (column == 0) {
+            if (column == 1) {
                 return ((Organization) node).getName();
             }
         }
