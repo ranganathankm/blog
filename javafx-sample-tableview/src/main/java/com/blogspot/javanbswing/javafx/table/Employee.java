1ef15fdf40d0e26f3282679ac9e8c951523e584f
ranga <ranga@ranga-home-ubuntu>
2014-11-21 16:26:20 +0530

    JavaFX TableView Sample - source code

---
+++ javafx-sample-tableview/src/main/java/com/blogspot/javanbswing/javafx/table/Employee.java
@@ -0,0 +1,78 @@
+package com.blogspot.javanbswing.javafx.table;
+
+import java.time.LocalDate;
+import javafx.beans.property.*;
+
+/**
+ *
+ * @author ranga
+ */
+
+public class Employee
+{
+    private SimpleIntegerProperty id;
+    private SimpleStringProperty name;
+    private SimpleDoubleProperty salary;
+    private SimpleBooleanProperty partTime;
+    private SimpleObjectProperty<LocalDate> doj;
+
+    public Employee(int id, String name, double salary, LocalDate doj, boolean partTime)
+    {
+        this.id = new SimpleIntegerProperty(id);
+        this.name = new SimpleStringProperty(name);
+        this.salary = new SimpleDoubleProperty(salary);
+        this.partTime = new SimpleBooleanProperty(partTime);
+        this.doj = new SimpleObjectProperty<>(doj);
+    }
+
+    public int getId()
+    {
+        return id.get();
+    }
+
+    public void setId(int id)
+    {
+        this.id.set(id);
+    }
+
+    public String getName()
+    {
+        return name.get();
+    }
+
+    public void setName(String name)
+    {
+        this.name.set(name);
+    }
+
+    public double getSalary()
+    {
+        return salary.get();
+    }
+
+    public void setSalary(double salary)
+    {
+        this.salary.set(salary);
+    }
+
+    public boolean isPartTime()
+    {
+        return partTime.get();
+    }
+
+    public void setPartTime(boolean partTime)
+    {
+        this.partTime.set(partTime);
+    }
+
+    public LocalDate getDoj()
+    {
+        return doj.get();
+    }
+
+    public void setDoj(LocalDate doj)
+    {
+        this.doj.set(doj);
+    }
+
+}
---
+++ javafx-sample-tableview/src/main/java/com/blogspot/javanbswing/javafx/table/TableViewSample.java
@@ -0,0 +1,77 @@
+package com.blogspot.javanbswing.javafx.table;
+
+import java.time.LocalDate;
+import javafx.application.*;
+import javafx.collections.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.util.converter.*;
+
+/**
+ *
+ * @author ranga
+ */
+
+public class TableViewSample extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        final Employee emp1 = new Employee(1, "Ram", 23123.23, LocalDate.now(), false);
+        final Employee emp2 = new Employee(2, "Krishna", 32398.76, LocalDate.now(), true);
+
+        final ObservableList<Employee> data
+                = FXCollections.observableArrayList(
+                        emp1, emp2);
+        
+        TableView<Employee> tableView = new TableView<>();        
+
+        TableColumn<Employee, Integer> idCol = new TableColumn<>("ID");
+        idCol.setCellValueFactory(
+                new PropertyValueFactory<>("id"));
+        
+        TableColumn<Employee, String> nameCol = new TableColumn<>("Name");
+        nameCol.setPrefWidth(100);
+        nameCol.setCellValueFactory(
+                new PropertyValueFactory<>("name"));
+        nameCol.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        TableColumn<Employee, Double> salaryCol = new TableColumn<>("Salary");
+        salaryCol.setPrefWidth(100);
+        salaryCol.setCellValueFactory(
+                new PropertyValueFactory<>("salary"));
+        salaryCol.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));
+
+        TableColumn<Employee, Boolean> partTimeCol = new TableColumn<>("Part Time?");
+        partTimeCol.setPrefWidth(100);
+        partTimeCol.setCellValueFactory(
+                new PropertyValueFactory<>("partTime"));
+        partTimeCol.setCellFactory(CheckBoxTableCell.forTableColumn(partTimeCol));
+
+        TableColumn<Employee, LocalDate> dojCol = new TableColumn<>("DOJ");
+        dojCol.setPrefWidth(100);
+        dojCol.setCellValueFactory(
+                new PropertyValueFactory<>("doj"));
+
+        tableView.getColumns().addAll(idCol, nameCol, salaryCol, partTimeCol, dojCol);
+
+        tableView.setItems(data);
+        tableView.setEditable(true);
+
+        StackPane root = new StackPane();
+        root.getChildren().add(tableView);
+        
+        Scene scene = new Scene(root, 450, 300);
+
+        stage.setTitle("JavaFX TableView Sample");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
