6fa07f5e440d36768aa08473eb0858426489b586
ranga <>
2021-07-09 19:43:57 +0530

    minor changes

--- nbdiff-client-test/src/main/java/com/blogspot/ranganathankm/nbdiff/client/test/TestBuitInDiffClient.java
+++ nbdiff-client-test/src/main/java/com/blogspot/ranganathankm/nbdiff/client/test/TestBuitInDiffClient.java
@@ -1,10 +1,10 @@
-package com.blogspot.javanbswing.nbdiff.client.test;
+package com.blogspot.ranganathankm.nbdiff.client.test;
 
 import java.io.*;
 import java.util.logging.*;
 import javax.swing.text.*;
 import org.netbeans.api.diff.Difference;
-import org.netbeans.modules.diff.cmdline.CmdlineDiffProvider;
+import org.netbeans.modules.diff.builtin.provider.BuiltInDiffProvider;
 import org.netbeans.modules.diff.util.*;
 import org.netbeans.spi.diff.DiffProvider;
 
@@ -12,35 +12,34 @@ import org.netbeans.spi.diff.DiffProvider;
  *
  * @author ranga
  */
-public class TestCmdLineDiffClient
+public class TestBuitInDiffClient
 {
 
     /**
      * Creates new form TestDiffClient
      */
-    public TestCmdLineDiffClient()
+    public TestBuitInDiffClient()
     {
     }
-
-    void showDiff(String filePath1, String filePath2)
-            throws FileNotFoundException, IOException, BadLocationException
+    
+    void showDiff(String filePath1, String filePath2) throws FileNotFoundException, IOException, BadLocationException
     {
         Difference[] differences;
         File file1 = new File(filePath1);
         File file2 = new File(filePath2);
-
+        
         Reader r1 = new FileReader(file1);
         Reader r2 = new FileReader(file2);
-
-        DiffProvider diff = new CmdlineDiffProvider(); //(DiffProvider) Lookup.getDefault().lookup(DiffProvider.class);        
+        
+        DiffProvider diff = new BuiltInDiffProvider(); //(DiffProvider) Lookup.getDefault().lookup(DiffProvider.class);        
         differences = diff.computeDiff(r1, r2);
-
+        
         r1.close();
         r2.close();
-
+        
         r1 = new FileReader(file1);
         r2 = new FileReader(file2);
-
+        
         TextDiffInfo info = new TextDiffInfo(
                 filePath1, filePath2, null, null, r1, r2, differences);
         info.setContextMode(true, 3);
@@ -49,25 +48,24 @@ public class TestCmdLineDiffClient
         System.out.println(norDiff);
     }
 
-    void showUnifiedDiff(String filePath1, String filePath2)
-            throws FileNotFoundException, IOException, BadLocationException
+    void showUnifiedDiff(String filePath1, String filePath2) throws FileNotFoundException, IOException, BadLocationException
     {
         Difference[] differences;
         File file1 = new File(filePath1);
         File file2 = new File(filePath2);
-
+        
         Reader r1 = new FileReader(file1);
         Reader r2 = new FileReader(file2);
-
-        DiffProvider diff = new CmdlineDiffProvider(); //(DiffProvider) Lookup.getDefault().lookup(DiffProvider.class);        
+        
+        DiffProvider diff = new BuiltInDiffProvider(); //(DiffProvider) Lookup.getDefault().lookup(DiffProvider.class);        
         differences = diff.computeDiff(r1, r2);
-
+        
         r1.close();
         r2.close();
 
         r1 = new FileReader(file1);
         r2 = new FileReader(file2);
-
+        
         TextDiffInfo info = new TextDiffInfo(
                 filePath1, filePath2, null, null, r1, r2, differences);
         info.setContextMode(true, 3);
@@ -81,11 +79,12 @@ public class TestCmdLineDiffClient
      */
     public static void main(String args[]) throws IOException, FileNotFoundException, BadLocationException
     {
-        TestCmdLineDiffClient testDiffClient = new TestCmdLineDiffClient();
+
+        final TestBuitInDiffClient testDiffClient = new TestBuitInDiffClient();
         //testDiffClient.showUnifiedDiff(TestBuitInDiffClient.class.getResource("/DiffTestFile1a.txt").getPath(), 
-          //                             TestBuitInDiffClient.class.getResource("/DiffTestFile1b.txt").getPath());
+                                       //TestBuitInDiffClient.class.getResource("/DiffTestFile1b.txt").getPath());
         testDiffClient.showDiff(TestBuitInDiffClient.class.getResource("/DiffTestFile1a.txt").getPath(), 
                                        TestBuitInDiffClient.class.getResource("/DiffTestFile1b.txt").getPath());
     }
-
+                 
 }
